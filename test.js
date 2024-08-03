b56db6dc5ec93ed1e5e7b117dceee5a3879a2dee
Bheempalli Sai Srinu <bheempalli.sai.srinu@ibm.com>
2024-08-03 15:43:50 +0530

    check delimiter data and build number list for addition

--- test.js
+++ test.js
@@ -4,6 +4,18 @@ try{
         if (numbers === '') {
             return 0;
         }
+
+        let delimiter = /,|\n/;
+        let numbersString = numbers;
+
+        if (numbers.startsWith('//')) {
+            const delimiterEndIndex = numbers.indexOf('\n');
+            delimiter = new RegExp(numbers.substring(2, delimiterEndIndex).replace(/[\[\]]/g, ''), 'g');
+            numbersString = numbers.substring(delimiterEndIndex + 1);
+        }
+
+        const numberList = numbersString.split(delimiter);
+        console.log(numberList)
     }
 console.log(add(1,2,3,4,5));
 } catch (e) {
